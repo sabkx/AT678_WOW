9d1401220a91a3b3317a0fc5161fdda3777ba63d
XingYue3419 <h2010141@nushigh.edu.sg>
2022-12-16 20:52:46 +0800

    STUFF

--- YQcode.cpp
+++ YQcode.cpp
@@ -1,7 +1,9 @@
 #include<bits/stdc++.h>
 #include<algorithm>
+#include <unistd.h>
+
 using namespace std;
-const int MAX_WIDTH = 10000, CHAR_MIN_SIZE = 16;
+const int MAX_HEIGHT = 65, MAX_WIDTH = 10000, CHAR_MIN_SIZE = 100;
 
 struct Image{
     int w, h;
@@ -28,7 +30,7 @@ struct Image{
 };
 
 void initBound(int &xMin, int &xMax, int &yMin, int &yMax, Image image);
-pair<Image, bool> connectedPixels(int x, int y, Image image);
+Image connectedPixels(int x, int y, Image &image);
 
 Image readImg(){
     Image image;
@@ -51,20 +53,20 @@ Image readImg(){
 
 void printImg(Image image){
     for (int row = 0; row < image.h; row++){
-        char str[image.w];
-        //printf("%s\n", str);
+//        char str[image.w];
         for (int col = 0; col < image.w; col++){
-            str[col] = image.pixels[row][col] ? '#' : '.';
-            //printf("%")
+//            str[col] = image.pixels[row][col] ? '#' : '.';
+            printf("%c", image.pixels[row][col] ? '#' : '.');
         }
-        printf("%s\n", str);
+//        printf("%s\n", str);
+        printf("\n");
     }
 }
 
 void rmvNoise(Image &image){
     Image tmpImage(image);
     int dx[] = {-1, 0, 1, -1, 0, 1, -1, 0, 1},
-        dy[] = {-1, -1, -1, 0, 0, 0, 1, 1, 1};
+            dy[] = {-1, -1, -1, 0, 0, 0, 1, 1, 1};
     for (int row = 0; row < image.h; row++){
         for (int col = 0; col < image.w; col++){
 
@@ -89,37 +91,43 @@ void initBound(int &xMin, int &xMax, int &yMin, int &yMax, Image image){
     yMax = 0;
 }
 
-pair<Image, bool> connectedPixels(int x, int y, Image image){
-    queue<pair<int, int>> connected;
+Image connectedPixels(int x, int y, Image &image){
+    return image; //return statement just for testing
+    //queue<pair<int, int>> connected;
+    printf("connectedPixels");
+    int vis[MAX_HEIGHT][MAX_WIDTH];
     queue<pair<int, int>> unchecked;
     int dx[] = {-1, 0, 0, 1},
-        dy[] = {0, -1, 1, 0};
+            dy[] = {0, -1, 1, 0};
     int xMin, xMax, yMin, yMax;
     initBound(xMin, xMax, yMin, yMax, image);
-
+    return image;
     printf("%d %d %d %d\n", xMin, xMax, yMin, yMax);
-    connected.push({x, y});
+    //connected.push({x, y});
     unchecked.push({x, y});
-    xMin = min(xMin, x); xMax = max(xMax, x);
-    yMin = min(yMin, y); yMax = max(yMax, y);
-    printf("%d %d %d %d\n", xMin, xMax, yMin, yMax);
     image.pixels[x][y] = 0;
+//    xMin = min(xMin, x), xMax = max(xMax, x);
+//    yMin = min(yMin, y), yMax = max(yMax, y);
+//    printf("%d %d %d %d\n", xMin, xMax, yMin, yMax);
+//    image.pixels[x][y] = 0;
+    return image;
     while (!unchecked.empty()){
         pair<int, int> chkPair = unchecked.front();
         unchecked.pop();
         printf("%d\n", unchecked.empty());
         int xChk = chkPair.first, yChk = chkPair.second;
+        xMin = min(xMin, xChk), xMax = max(xMax, xChk);
+        yMin = min(yMin, yChk), yMax = max(yMax, yChk);
+        vis[xChk][yChk] = 1;
+        printf("(%d, %d) %d %d %d %d\n", xChk, yChk, xMin, xMax, yMin, yMax);
+
         for (int i = 0; i < 4; i++){
             int nx = xChk + dx[i], ny = yChk + dy[i];
-            if (!image.inRange(nx, ny)) continue;
-            printf("(%d, %d) %d %d %d %d\n", nx, ny, xMin, xMax, yMin, yMax);
-            if (image.pixels[nx][ny]){
+            if (image.inRange(nx, ny) && image.pixels[nx][ny]){
                 printf("in\n");
-                printf("%d\n", connected.size());
-                connected.push({nx, ny});
+                //printf("%d\n", connected.size());
+                //connected.push({nx, ny});
                 unchecked.push({nx, ny});
-                xMin = min(xMin, nx); xMax = max(xMax, nx);
-                yMin = min(yMin, ny); yMax = max(yMax, ny);
                 image.pixels[nx][ny] = 0;
                 //printImg(image);
             }
@@ -129,28 +137,32 @@ pair<Image, bool> connectedPixels(int x, int y, Image image){
     printf("here\n\n\n");
 
     Image charImage;
-    bool valid = connected.size() >= CHAR_MIN_SIZE;
     charImage.h = xMax - xMin + 1;
     charImage.w = yMax - yMin + 1;
-    image.pixels = vector<vector<int>>(charImage.h, vector<int>(charImage.w, 0));
-    while (!connected.empty()){
-        pair<int, int> pair = connected.front();
-        connected.pop();
-        int xChk = pair.first, yChk = pair.second;
-        charImage.pixels[xChk - xMin][yChk - yMin] = 1;
+    image.pixels = vector<vector<int>>(charImage.h, vector<int>(charImage.w));
+    for (int row = 0; row < image.h; row++){
+        for (int col = 0; col < image.w; col++){
+            charImage.pixels[row][col] = vis[row + xMin][col + yMin];
+        }
+//        pair<int, int> pair = connected.front();
+//        connected.pop();
+//        int xChk = pair.first, yChk = pair.second;
+//        charImage.pixels[xChk - xMin][yChk - yMin] = 1;
     }
 
-    return {charImage, valid};
+    return charImage;
 }
 
-vector<Image> splitExpr(Image image){
+vector<Image> splitExpr(Image &image){
+    printf("splitExpr()");
     vector<Image> charList;
     for (int col = 0; col < image.w; col++){
         for (int row = 0; row < image.h; row++){
             if (image.pixels[row][col]){
-                pair<Image, bool> pair = connectedPixels(row, col, image);
-                if (pair.second){
-                    charList.push_back(pair.first);
+                Image charImage = connectedPixels(row, col, image);
+                return charList;
+                if (charImage.h * charImage.w >= CHAR_MIN_SIZE){
+                    charList.push_back(charImage);
                     printf("char finished\n\n");
                 }
             }
@@ -161,16 +173,22 @@ vector<Image> splitExpr(Image image){
 }
 
 int main() {
+    printf("why?");
     Image i = readImg();
+    //printImg(i);
     printf("\n\n\n");
     rmvNoise(i);
-    printImg(i);
+    printf("hello?");
+    //printImg(i);
     printf("\n\n\n");
-    vector<Image> charList = splitExpr(i);
-    for (int i = 0; i < charList.size(); i++){
+    //sleep(5);
+    //*
+    vector<Image> charList = splitExpr(i);/* //Disable this line and printf works
+    for (int i = 0; i < charList.size(); i++) {
         printImg(charList[i]);
         printf("\n");
     }
+    */
     printf("done\n\n\n");
     return 0;
 }

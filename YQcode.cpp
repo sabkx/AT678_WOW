fc36b0eb0178f7694a15435970973610a1929d71
XingYue3419 <h2010141@nushigh.edu.sg>
2022-12-19 12:21:15 +0800

    STUFF

--- YQcode.cpp
+++ YQcode.cpp
@@ -4,14 +4,25 @@
 
 using namespace std;
 const int MAX_HEIGHT = 65, MAX_WIDTH = 10000, CHAR_MIN_SIZE = 100;
+int vis[MAX_HEIGHT][MAX_WIDTH];
 
 struct Image{
     int w, h;
     vector<vector<int>> pixels;
     Image(){}
+    Image(int h, int w){
+        this->h = h;
+        this->w = w;
+        pixels = vector<vector<int>>(h, vector<int>(w, 0));
+        for (int row = 0; row < h; row++){
+            for (int col = 0; col < w; col++){
+                pixels[row][col] = 0;
+            }
+        }
+    }
     Image(Image const &image){
-        w = image.w;
         h = image.h;
+        w = image.w;
         pixels = vector<vector<int>>(h, vector<int>(w));
         for (int row = 0; row < h; row++){
             for (int col = 0; col < w; col++){
@@ -33,12 +44,9 @@ void initBound(int &xMin, int &xMax, int &yMin, int &yMax, Image image);
 Image connectedPixels(int x, int y, Image &image);
 
 Image readImg(){
-    Image image;
     int w, h;
     scanf("%d %d", &w, &h);
-    image.w = w;
-    image.h = h;
-    image.pixels = vector<vector<int>>(h, vector<int>(w));
+    Image image(h, w);
     for (int row = 0; row < h; row++){
         char str[MAX_WIDTH];
         scanf("%s", &str);
@@ -56,6 +64,10 @@ void printImg(Image image){
 //        char str[image.w];
         for (int col = 0; col < image.w; col++){
 //            str[col] = image.pixels[row][col] ? '#' : '.';
+//            if (image.pixels[row][col] != 1 || image.pixels[row][col] != 0){
+//                printf("%d %d %d\n", image.pixels[row][col], row, col);
+//                return;
+//            }
             printf("%c", image.pixels[row][col] ? '#' : '.');
         }
 //        printf("%s\n", str);
@@ -92,17 +104,15 @@ void initBound(int &xMin, int &xMax, int &yMin, int &yMax, Image image){
 }
 
 Image connectedPixels(int x, int y, Image &image){
-    return image; //return statement just for testing
     //queue<pair<int, int>> connected;
-    printf("connectedPixels");
-    int vis[MAX_HEIGHT][MAX_WIDTH];
+    printf("connectedPixels()\n");
+
     queue<pair<int, int>> unchecked;
     int dx[] = {-1, 0, 0, 1},
             dy[] = {0, -1, 1, 0};
     int xMin, xMax, yMin, yMax;
     initBound(xMin, xMax, yMin, yMax, image);
-    return image;
-    printf("%d %d %d %d\n", xMin, xMax, yMin, yMax);
+    //printf("%d %d %d %d\n", xMin, xMax, yMin, yMax);
     //connected.push({x, y});
     unchecked.push({x, y});
     image.pixels[x][y] = 0;
@@ -110,21 +120,20 @@ Image connectedPixels(int x, int y, Image &image){
 //    yMin = min(yMin, y), yMax = max(yMax, y);
 //    printf("%d %d %d %d\n", xMin, xMax, yMin, yMax);
 //    image.pixels[x][y] = 0;
-    return image;
     while (!unchecked.empty()){
         pair<int, int> chkPair = unchecked.front();
         unchecked.pop();
-        printf("%d\n", unchecked.empty());
+        //printf("%d\n", unchecked.empty());
         int xChk = chkPair.first, yChk = chkPair.second;
         xMin = min(xMin, xChk), xMax = max(xMax, xChk);
         yMin = min(yMin, yChk), yMax = max(yMax, yChk);
         vis[xChk][yChk] = 1;
-        printf("(%d, %d) %d %d %d %d\n", xChk, yChk, xMin, xMax, yMin, yMax);
+        //printf("(%d, %d) %d %d %d %d\n", xChk, yChk, xMin, xMax, yMin, yMax);
 
         for (int i = 0; i < 4; i++){
             int nx = xChk + dx[i], ny = yChk + dy[i];
             if (image.inRange(nx, ny) && image.pixels[nx][ny]){
-                printf("in\n");
+                //printf("in\n");
                 //printf("%d\n", connected.size());
                 //connected.push({nx, ny});
                 unchecked.push({nx, ny});
@@ -135,35 +144,34 @@ Image connectedPixels(int x, int y, Image &image){
     }
 
     printf("here\n\n\n");
-
-    Image charImage;
-    charImage.h = xMax - xMin + 1;
-    charImage.w = yMax - yMin + 1;
-    image.pixels = vector<vector<int>>(charImage.h, vector<int>(charImage.w));
-    for (int row = 0; row < image.h; row++){
-        for (int col = 0; col < image.w; col++){
+    Image charImage(xMax - xMin + 1, yMax - yMin + 1);
+    for (int row = 0; row < charImage.h; row++){
+        for (int col = 0; col < charImage.w; col++){
+            //printf("%d %d ", charImage.pixels[row][col], vis[row + xMin][col + yMin]);
             charImage.pixels[row][col] = vis[row + xMin][col + yMin];
+            vis[row + xMin][col + yMin] = 0;
         }
+        //printf("\n");
 //        pair<int, int> pair = connected.front();
 //        connected.pop();
 //        int xChk = pair.first, yChk = pair.second;
 //        charImage.pixels[xChk - xMin][yChk - yMin] = 1;
     }
-
     return charImage;
 }
 
 vector<Image> splitExpr(Image &image){
-    printf("splitExpr()");
+    printf("splitExpr()\n");
     vector<Image> charList;
     for (int col = 0; col < image.w; col++){
         for (int row = 0; row < image.h; row++){
             if (image.pixels[row][col]){
                 Image charImage = connectedPixels(row, col, image);
-                return charList;
                 if (charImage.h * charImage.w >= CHAR_MIN_SIZE){
+                    printf("h: %d, w: %d", charImage.h, charImage.w);
                     charList.push_back(charImage);
                     printf("char finished\n\n");
+                    //return charList;
                 }
             }
         }
@@ -173,22 +181,17 @@ vector<Image> splitExpr(Image &image){
 }
 
 int main() {
-    printf("why?");
     Image i = readImg();
-    //printImg(i);
-    printf("\n\n\n");
     rmvNoise(i);
-    printf("hello?");
-    //printImg(i);
+    printImg(i);
     printf("\n\n\n");
-    //sleep(5);
     //*
-    vector<Image> charList = splitExpr(i);/* //Disable this line and printf works
+    vector<Image> charList = splitExpr(i);//* //Disable this line and printf works
     for (int i = 0; i < charList.size(); i++) {
         printImg(charList[i]);
         printf("\n");
     }
-    */
+    //*/
     printf("done\n\n\n");
     return 0;
 }

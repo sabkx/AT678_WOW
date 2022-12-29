45d6988d2869f5d08101935515642ef25f5f3a04
XingYue3419 <h2010141@nushigh.edu.sg>
2022-12-29 18:27:43 +0800

    STUFF

--- YQStuff/cmake-build-debug/cmake_install.cmake
+++ YQStuff/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/TANYIQUAN/CLionProjects/W@W_Project
+# Install script for directory: C:/Users/TANYIQUAN/Documents/GitHub/AT678_WOW/YQStuff
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/TANYIQUAN/CLionProjects/W@W_Project/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/TANYIQUAN/Documents/GitHub/AT678_WOW/YQStuff/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
